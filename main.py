250e5d713f923a0fc92e18945bddb05b845ba462
Joey Kong <roo.joey@gmail.com>
2024-03-15 14:49:43 +1100

    add Execution time

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ from llama_index.core import Settings
 from llama_index.llms.replicate import Replicate
 from llama_index.embeddings.huggingface import HuggingFaceEmbedding
 from transformers import AutoTokenizer
-from llama_index.llms.replicate import Replicate
 from prompts import system_prompt, query_wrapper_prompt
+import datetime
 
 load_dotenv()
 
@@ -41,10 +41,10 @@ load_dotenv()
 
 
 # The replicate endpoint
-LLAMA_13B_V2_CHAT = "a16z-infra/llama13b-v2-chat:df7690f1994d94e96ad9d568eac121aecf50684a0b0963b25a41cc40061269e5"
+# LLAMA_13B_V2_CHAT = "a16z-infra/llama13b-v2-chat:df7690f1994d94e96ad9d568eac121aecf50684a0b0963b25a41cc40061269e5"
 
 # set the LLM
-system_prompt = "Your system prompt here"  # Replace "Your system prompt here" with the desired value
+# system_prompt = "Your system prompt here"  # Replace "Your system prompt here" with the desired value
 
 llama2_7b_chat = "meta/llama-2-7b-chat:8e6975e5ed6174911a6ff3d60540dfd4844201974602551e10e9e87ab143d81e"
 Settings.llm = Replicate(
@@ -72,6 +72,13 @@ pdf_indexer = PDFIndexer("canada", Settings.llm, Settings.embed_model)
 canada_engine = pdf_indexer.index_pdf(os.path.join("data", "Canada.pdf"))
 
 while (prompt := input("Enter a prompt (q to quit): ")) != "q":
+    start_time = datetime.datetime.now()
+    
     result = canada_engine.query(prompt)
+    
+    end_time = datetime.datetime.now()
+    execution_time = end_time - start_time
+    
     print(result)
+    print("Execution time:", execution_time.total_seconds())
     
